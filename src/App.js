82e492bbf300d3d0c632dd65c15e0c5dd9fffe01
Shinki7 <alexaten007@gmail.com>
2022-12-09 14:23:50 +0700

    SASAAWAWEae

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Select from "react-select";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [datas, setDatas] = useState([])
+  const [userSelect, setUserSelect] = useState("")
+  const [isShow, setIsShow] = useState(false)
+
+  const getcov = async () => {
+    const cov = await fetch("https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi")
+      const value = await cov.json()
+      const result = value.map(data => {
+      console.log(data)
+        return{
+          label: data.provinsi,
+          value: data.dirawat,
+          value: data.kasus
+        }
+      })
+      setDatas(result)
+  }
+
+  useEffect(() => {
+    getcov()
+  }, [])
+
+  const handleChange = (value) => {
+    setUserSelect(value)
+  }
+  const handleSubmit = () => {
+      setIsShow(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select options={datas} onChange={(e) => handleChange(e.value)}></Select>
+      <br></br>
+      <button onClick={() => handleSubmit()}>Tampilkan Data</button>
+      <h1>Jumlah Kasus Covid: {isShow ? userSelect : ""} Kasus</h1>
     </div>
   );
 }
